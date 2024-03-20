2e72a192f134724c0e87d6f528c594126c50ae7a
bagasdhitya98 <bagasdhityataufiqqi21@gmail.com>
2024-03-20 20:22:43 +0700

    add: create adapters for routing api

---
+++ src/adapters/httpAdapter.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const catchPokemon = require("../usecases/catchPokemon");
+const releasePokemon = require("../usecases/releasePokemon");
+const renamePokemon = require("../usecases/renamePokemon");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/catch-pokemon", (req, res) => {
+  const result = catchPokemon();
+  res.json(result);
+});
+
+app.post("/release-pokemon", (req, res) => {
+  const { number } = req.body;
+  const result = releasePokemon(number);
+  res.json(result);
+});
+
+app.post("/rename-pokemon", (req, res) => {
+  const { name } = req.body;
+  const result = renamePokemon(name);
+  res.json(result);
+});
+
+module.exports = app;
